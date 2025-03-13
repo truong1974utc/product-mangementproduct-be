abc1b2376f58ef24c420861898d95283387e4c91
truong1974utc <truong1974tdbn@gmail.com>
2025-03-14 01:35:55 +0700

    Xay dung layouts chung cho admin

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const database = require("./config/database")
 
+const systemConfig = require("./config/system")
+
 const route = require("./routes/client/index.route")
 const routeAdmin = require("./routes/admin/index.route")
 
@@ -13,6 +15,9 @@ const port = process.env.PORT
 
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+// App Locals Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
 
 app.use(express.static("public"))
 
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,5 +3,5 @@ const systemConfig = require("../../config/system")
 
 module.exports = (app) => {
     const  PATH_ADMIN = systemConfig.prefixAdmin
-    app.use(PATH_ADMIN, dashboardRoutes)
+    app.use(PATH_ADMIN + "/dashboard", dashboardRoutes)
 }
