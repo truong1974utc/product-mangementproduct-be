92d55489e339bdfed5d736f226a8461e4ce49649
truong1974utc <truong1974tdbn@gmail.com>
2025-03-18 17:51:09 +0700

    Lay data sp in ra ngoai giao dien

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,14 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model")
+
+// [GET] /admin/products
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+    
     res.render("admin/pages/products/index", {
-        pageTitle: "Trang san pham"
+        pageTitle: "Trang san pham",
+        products: products
     })
 }
 
