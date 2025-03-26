14368f307ddf95e05ee425dfda0fd8f76b27dce8
truong1974utc <truong1974tdbn@gmail.com>
2025-03-26 21:53:43 +0700

    Tinh nang tim kiem

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -16,3 +16,23 @@ if(buttonsStatus.length > 0){
     })
 }
 // End Button Status
+
+// Form Search
+const formSearch = document.querySelector("#form-search")
+if(formSearch) {
+    let url = new URL(window.location.href)
+
+    formSearch.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const keyword = e.target.elements.keyword.value;
+
+        if(keyword) {
+            url.searchParams.set("keyword", keyword)
+        }else {
+            url.searchParams.delete("keyword")
+        }
+
+        window.location.href = url.href
+    })
+}
+// End Form Search
