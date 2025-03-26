d33b755137cc90440e1361ca0e932952b01d0fff
truong1974utc <truong1974tdbn@gmail.com>
2025-03-26 19:05:27 +0700

    Tinh nang loc theo trang thai

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,13 +2,47 @@ const Product = require("../../models/product.model")
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+
+    let filterStatus = [
+        {
+            name: "Tat Ca",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoat Dong",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dung Hoat Dong",
+            status: "inactive",
+            class: ""
+        }
+    ]
+
+    if(req.query.status) {
+        const index = filterStatus.findIndex(item => item.status == req.query.status)
+        filterStatus[index].class = "active"
+    }else {
+        const index = filterStatus.findIndex(item => item.status == "")
+        filterStatus[index].class = "active"
+    }
+
+    let find = {
         deleted: false
-    })
+    }
+
+    if(req.query.status) {
+        find.status = req.query.status
+    }
+
+    const products = await Product.find(find)
     
     res.render("admin/pages/products/index", {
         pageTitle: "Trang san pham",
-        products: products
+        products: products,
+        filterStatus: filterStatus
     })
 }
 
