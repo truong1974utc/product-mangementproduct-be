97775f5cd22e9eb35d718e4f441db6153e235a1c
truong1974utc <truong1974tdbn@gmail.com>
2025-05-02 17:52:26 +0700

    Thay đổi trạng thái 1 sp

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -46,3 +46,12 @@ module.exports.index = async (req, res) => {
     })
 }
 
+// [GET] /admin/products/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+    const status = req.params.status
+    const id = req.params.id;
+    
+    await Product.updateOne({_id: id}, { status: status})
+
+    res.redirect("back")
+}
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,5 +3,6 @@ const route = express.Router()
 const controller = require("../../controllers/admin/product.controller")
 
 route.get("/", controller.index)
+route.patch("/change-status/:status/:id", controller.changeStatus)
 
 module.exports = route
