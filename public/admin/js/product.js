fcbe8ca57c36ba911104c0afe0bd815443d66dc3
truong1974utc <truong1974tdbn@gmail.com>
2025-05-03 23:13:16 +0700

    Xoa 1 san pham

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -18,3 +18,23 @@ if(buttonChangeStatus.length > 0){
     })
 }
 // End Change Status
+
+// Delete Item
+const buttonsDelete = document.querySelectorAll("[button-delete]")
+if(buttonsDelete.length > 0) {
+    const formDeleteItem = document.querySelector("#form-delete-item")
+    const path = formDeleteItem.getAttribute("data-path")
+
+    buttonsDelete.forEach(button => {
+        button.addEventListener("click", () => {
+            const isConfirm = confirm("Ban co chac muon xoa san pham nay")
+            if(isConfirm) {
+                const id = button.getAttribute("data-id")
+                const action = `${path}/${id}?_method=DELETE`
+                formDeleteItem.action = action
+                formDeleteItem.submit()
+            }
+        })
+    })
+}
+// End Delete Item
