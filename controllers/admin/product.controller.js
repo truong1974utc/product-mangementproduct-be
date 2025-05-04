780443e16fa5ac81cd972c3f8e15a8650538473c
truong1974utc <truong1974tdbn@gmail.com>
2025-05-04 22:25:17 +0700

    tinh nang thay doi vi tri sp

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -35,7 +35,10 @@ module.exports.index = async (req, res) => {
     
     // End Pagination
 
-    const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip)
+    const products = await Product.find(find)
+        .sort({position: "desc"})
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip)
     
     res.render("admin/pages/products/index", {
         pageTitle: "Trang san pham",
@@ -69,7 +72,15 @@ module.exports.changeMulti = async (req, res) => {
             await Product.updateMany({ _id: {$in: ids}}, {status: "inactive"})
             break;
         case "delete-all":
-            await Product.updateMany({_id: {$in: ids}}, {deleted: true, deletedAt: new Date()})    
+            await Product.updateMany({_id: {$in: ids}}, {deleted: true, deletedAt: new Date()})
+            break;
+        case "change-position":
+            for(const item of ids){
+                let[id, position] = item.split("-");
+                position = parseInt(position)
+                await Product.updateOne({_id: id}, {position: position})
+            }
+            break;       
         default:
             break;
     }
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -110,7 +110,16 @@ if(formChangeMulti) {
 
             inputsChecked.forEach(input => {
                 const id = input.value
-                ids.push(id)
+                
+                if(typeChange == "change-position") {
+                    const position = input
+                        .closest("tr")
+                        .querySelector("input[name='position']").value
+
+                    ids.push(`${id}-${position}`)
+                } else {
+                    ids.push(id)
+                }
             })
 
             inputIds.value = ids.join(", ")
