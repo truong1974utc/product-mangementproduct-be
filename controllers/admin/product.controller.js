c7489fbccab3b16a53ecaaca0f25690cd7941c3a
truong1974utc <truong1974tdbn@gmail.com>
2025-05-05 16:06:45 +0700

    Hiển thị thông báo khi thay đổi trạng thái sp

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -56,6 +56,8 @@ module.exports.changeStatus = async (req, res) => {
     
     await Product.updateOne({_id: id}, { status: status})
 
+    req.flash("success", "Cap nhat trang thai thanh cong");
+
     res.redirect("back")
 }
 
@@ -67,9 +69,12 @@ module.exports.changeMulti = async (req, res) => {
     switch (type) {
         case "active":
             await Product.updateMany({ _id: {$in: ids}}, {status: "active"})
+            req.flash("success", `Cap nhat trang thai thanh cong ${ids.length} san pham !`)
             break;
         case "inactive":
             await Product.updateMany({ _id: {$in: ids}}, {status: "inactive"})
+            req.flash("success", `Cap nhat trang thai thanh cong ${ids.length} san pham !`)
+
             break;
         case "delete-all":
             await Product.updateMany({_id: {$in: ids}}, {deleted: true, deletedAt: new Date()})
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -131,3 +131,19 @@ if(formChangeMulti) {
     });
 }
 // End Form Change Multi
+
+// show alert
+const showAlert = document.querySelector("[show-alert]")
+if(showAlert) {
+    const time = parseInt(showAlert.getAttribute("data-time"))
+    const closeAlert = showAlert.querySelector("[close-alert]")
+
+    setTimeout(() => {
+        showAlert.classList.add("alert-hidden");
+    }, time);
+
+    closeAlert.addEventListener("click", () => {
+        showAlert.classList.add("alert-hidden")
+    })
+}
+// end show alert
