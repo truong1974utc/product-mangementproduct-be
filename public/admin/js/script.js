0cdf22cd17169a4b1fee3ee0493775b3cfc44d14
truong1974utc <truong1974tdbn@gmail.com>
2025-05-07 21:30:02 +0700

    Tinh nang tao moi san pham (hien anh khi them anh)

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -147,3 +147,19 @@ if(showAlert) {
     })
 }
 // end show alert
+
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]")
+if(uploadImage) {
+    const uploadImageInput = document.querySelector("[upload-image-input]");
+    const uploadImagePreview = document.querySelector("[upload-image-preview]")
+
+    uploadImageInput.addEventListener("change", (e) => {
+        console.log(e);
+        const file = e.target.files[0]
+        if(file) {
+            uploadImagePreview.src = URL.createObjectURL(file)
+        }
+    })
+}
+// End Upload Image
