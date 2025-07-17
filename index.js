6dc53425e4947560660b02fa457111ebb6cab716
truong1974utc <truong1974tdbn@gmail.com>
2025-07-17 18:54:50 +0700

    Tiny MCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path');
 const methodOverride = require('method-override')
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
@@ -32,6 +33,10 @@ app.use(cookieParser('NGUYENQUANGTRUONG'));
 app.use(session({ cookie: { maxAge: 60000 } }));
 app.use(flash());
 // End Flash
+
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End TinyMCE
 
 // App Locals Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin
