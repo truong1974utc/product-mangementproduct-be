73bd811ec8cbeefef4be386cdf183648b5a93c66
truong1974utc <truong1974tdbn@gmail.com>
2025-08-10 17:16:49 +0700

    Xây dựng phần phân quyền

--- routes/admin/role.route.js
+++ routes/admin/role.route.js
@@ -12,4 +12,8 @@ route.get("/edit/:id", controller.edit);
 
 route.patch("/edit/:id", controller.editPatch);
 
+route.get("/permissions", controller.permissions);
+
+route.patch("/permissions", controller.permissionsPatch);
+
 module.exports = route
