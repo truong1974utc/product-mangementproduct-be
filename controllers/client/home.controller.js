707b2838b967b350a549f97260150f0d42623a00
truong1974utc <truong1974tdbn@gmail.com>
2025-09-04 23:43:49 +0700

    Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,8 +1,19 @@
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
 
 // [GET] /
 module.exports.index = async (req, res) => {
+    // Lấy ra sản phảm nổi bật
+    const productsFeatured = await Product.find({
+        featured: "1",
+        deleted: false,
+        status: "active"
+    }).limit(3)
+
+    const newProducts = productsHelper.priceNewProducts(productsFeatured);
 
     res.render("client/pages/home/index", {
-        pageTitle: "Trang chủ"
+        pageTitle: "Trang chủ",
+        productsFeatured: newProducts
     })
 }
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
 
 // [GET] /products
 module.exports.index = async (req, res) => {
@@ -8,13 +9,10 @@ module.exports.index = async (req, res) => {
         deleted: false
     }).sort({position: "desc"});
     
-    const newProducts = products.map(item => {
-        item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
-        return item
-    })
+    const newProducts = productsHelper.priceNewProducts(products)
 
     res.render("client/pages/products/index", {
-        pageTitle: "Trang san pham",
+        pageTitle: "Trang sản phẩm",
         products: newProducts
     })
 }
