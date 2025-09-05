6967d305121f40a1955f45371f920f344b31cacd
truong1974utc <truong1974tdbn@gmail.com>
2025-09-05 11:09:09 +0700

    Hiển thị danh sách mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -10,10 +10,21 @@ module.exports.index = async (req, res) => {
         status: "active"
     }).limit(3)
 
-    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
+    // End Lấy ra sản phẩm nổi bật
+
+    // Hiển thị danh sách sản phẩm mới nhất
+    const productsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({ position: "desc"}).limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(productsNew)
+    // End Hiển thị danh sách sản phẩm mới nhất
 
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProducts
+        productsFeatured: newProductsFeatured,
+        productsNew: newProductsNew
     })
 }
