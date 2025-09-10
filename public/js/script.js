b6a0e0cc7098e136c686541e5e2a77079905b32b
truong1974utc <truong1974tdbn@gmail.com>
2025-09-10 18:59:45 +0700

    Thêm sản phẩm vào giỏ hàng

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
-console.log("Ok")
+// show alert
+const showAlert = document.querySelector("[show-alert]")
+if(showAlert) {
+    const time = parseInt(showAlert.getAttribute("data-time"))
+    const closeAlert = showAlert.querySelector("[close-alert]")
+
+    setTimeout(() => {
+        showAlert.classList.add("alert-hidden");
+    }, time);
+
+    closeAlert.addEventListener("click", () => {
+        showAlert.classList.add("alert-hidden")
+    })
+}
+// end show alert
