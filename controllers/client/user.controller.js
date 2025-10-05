9c24f991e2bc8de8cfdc3b6e11fdbd068d3cfc4a
truong1974utc <truong1974tdbn@gmail.com>
2025-10-05 23:50:04 +0700

    Quên mật khẩu

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,5 +1,8 @@
 const User = require("../../models/user.model")
+const ForgotPassword = require("../../models/forfot-password.model")
 const md5 = require("md5")
+
+const generateHelper = require("../../helpers/generate");
 
 // [GET] /user/register
 module.exports.register = async (req, res) => {
@@ -79,4 +82,76 @@ module.exports.loginPost = async (req, res) => {
 module.exports.logout = async (req, res) => {
     res.clearCookie("tokenUser");
     res.redirect("/")
+}
+
+// [GET] /user/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    res.render("client/pages/user/forgot-password", {
+        pageTitle: "lấy lại mật khẩu",
+    })
+}
+
+// [POST] /user/password/forgot
+module.exports.forgotPasswordPost = async (req, res) => {
+    const email = req.body.email;
+    
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    })
+
+    if(!user) {
+        req.flash("error", `Email không tồn tại`);
+        res.redirect("back");
+        return;
+    }
+
+    // Việc 1: Tạo mã OTP và lưu OTP, email vào collection forgot-password
+    const otp = generateHelper.generateRandomNumber(8);
+
+    const objectForgotPassword = {
+        email: email,
+        otp: otp,
+        expireAt: Date.now()
+    };
+
+    const forgotPassword = new ForgotPassword(objectForgotPassword)
+    await forgotPassword.save()
+
+    res.redirect(`/user/password/otp?email=${email}`);
+}
+
+// [GET] /user/password/otp
+module.exports.otpPassword = async (req, res) => {
+    const email = req.query.email;
+
+    res.render("client/pages/user/otp-password", {
+        pageTitle: "Nhập mã OTP",
+        email: email
+    });
+}
+
+// [POST] /user/password/otp
+module.exports.otpPasswordPost = async (req, res) => {
+    const email = req.body.email
+    const otp = req.body.otp
+
+    const result = await ForgotPassword.findOne({
+        email: email,
+        otp: otp
+    })
+
+    if(!result) {
+        req.flash("error", `OTP không hợp lệ`);
+        res.redirect("back")
+        return;
+    }
+
+    const user = await User.findOne({
+        email: email
+    })
+
+    res.cookie("tokenUser", user.tokenUser);
+
+    res.redirect("/user/password/reset")
 }
--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -13,6 +13,12 @@ route.post("/login", validate.loginPost, controller.loginPost)
 
 route.get("/logout", controller.logout)
 
+route.get("/password/forgot", controller.forgotPassword)
 
+route.post("/password/forgot", validate.forgotPasswordPost, controller.forgotPasswordPost)
+
+route.get("/password/otp", controller.otpPassword)
+
+route.post("/password/otp", controller.otpPasswordPost)
 
 module.exports = route
